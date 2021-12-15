27afff4da0965825cba46ef818d54016bb8c7344
Azay <azay.genius8@gmail.com>
2021-12-15 02:06:14 +0100

    Changed view to navigation container

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Text, View,Image, LayoutAnimation } from 'react-native';
 import Landing from './components/homepage/landing';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native' 
+
+
+const Stack =createNativeStackNavigator();
 
 
 export default function App() {
   return (
-    <View>
-     <Landing/>
+  <NavigationContainer>
+    <Stack.Navigator>
+
+    </Stack.Navigator>
+    
+  </NavigationContainer>
+  
    
-    </View>
+ 
   );
 }
