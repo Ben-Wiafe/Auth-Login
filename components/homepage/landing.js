264592ee32f464c666cbeb96b61f6fdc15988bfb
Azay <azay.genius8@gmail.com>
2021-12-15 11:44:11 +0100

    default landing page

--- components/homepage/landing.js
+++ components/homepage/landing.js
@@ -1,15 +1,23 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TouchableHighlight } from "react-native";
 import { useWindowDimensions } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import Firsthome from "./firsthome";
 
-export default function Landing() {
-
-    const {height, width} = useWindowDimensions();
+export default function Landing({ navigation }) {
+  const { height, width } = useWindowDimensions();
   return (
     <View>
-       <Image source={require('../../assets/splash.png')} style={{height:height, width:width/1, marginTop:height/19}} />
-    
+      <TouchableHighlight
+        onPress={() => navigation.navigate("Firsthome")}
+        activeOpacity={0.6}
+        underlayColor="#DDDDDD"
+      >
+        <Image
+          source={require("../../assets/splash.png")}
+          style={{ height: height, width: width / 1, marginTop: height / 19 }}
+        />
+      </TouchableHighlight>
     </View>
- 
-   );
+  );
 }
