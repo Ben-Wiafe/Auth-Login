331e3d64ba2ff8e4ae20b60c334e97a71da92fb7
Azay <azay.genius8@gmail.com>
2021-12-15 11:39:06 +0100

    Stack screens created

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Text, View,Image, LayoutAnimation } from 'react-native';
 import Landing from './components/homepage/landing';
+import Firsthome from './components/homepage/firsthome';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native' 
-
-
+import SecondHome from './components/homepage/secHome';
+import Login from './components/login/login';
+import SignIn from './components/login/signin';
+import SignUp from './components/login/signup';
 const Stack =createNativeStackNavigator();
 
 
@@ -12,9 +15,15 @@ export default function App() {
   return (
   <NavigationContainer>
     <Stack.Navigator>
+      <Stack.Screen name='Landing'  component={Landing} options={{ headerShown: false }}/>
+      <Stack.Screen name='Firsthome' component= {Firsthome} options={{headerShown:false}}/>
+      <Stack.Screen name='SecondHome' component= {SecondHome} options={{headerShown:false}}/>
+      <Stack.Screen name='Login' component= {Login} options={{headerShown:false}}/>
+      <Stack.Screen name='SignUp' component= {SignUp} options={{headerShown:false}}/>
+      <Stack.Screen name='SignIn' component= {SignIn} options={{headerShown:false}}/>
 
     </Stack.Navigator>
-    
+
   </NavigationContainer>
   
    
