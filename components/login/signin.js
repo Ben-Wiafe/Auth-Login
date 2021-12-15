23b9d6cdc523de9ee3afd9460d082f01ec97305f
Azay <azay.genius8@gmail.com>
2021-12-15 11:43:48 +0100

    Sign in screen

---
+++ components/login/signin.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Text, View, Image, TouchableOpacity } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { useWindowDimensions } from "react-native";
+
+const SignIn = ({ navigation }) => {
+  const { height, width } = useWindowDimensions();
+
+  return (
+    <View>
+      <View
+        style={{
+          marginTop: 130,
+          flex: 1,
+          alignItems: "center",
+          backgroundColor: "white",
+        }}
+      >
+        <Image source={require("../../assets/uLeanerlogo.png")} />
+      </View>
+      <View style={{ marginTop: 40 }}>
+        <Text
+          style={{
+            fontWeight: "bold",
+            fontSize: 35,
+            padding:40,
+            color: "black",
+          }}
+        >
+          Sign in
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+export default SignIn;
