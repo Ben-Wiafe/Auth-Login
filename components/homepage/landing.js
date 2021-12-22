44ecec794a699c1851b58a4f27c8de14c6c3494f
Azay <azay.genius8@gmail.com>
2021-12-22 05:43:50 +0100

    update colors and forms

--- components/homepage/landing.js
+++ components/homepage/landing.js
@@ -1,21 +1,44 @@
 import React from "react";
-import { View, Text, Image, TouchableHighlight } from "react-native";
-import { useWindowDimensions } from "react-native";
+import { View, Text, Image,TouchableOpacity, StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 import { NavigationContainer } from "@react-navigation/native";
-import Firsthome from "./firsthome";
+import Firsthome from '././firsthome'
 
-export default function Landing({ navigation }) {
-  const { height, width } = useWindowDimensions();
+export default function Landing({navigation}) {
   return (
-    <View>
-      <TouchableHighlight
-        onPress={() => navigation.navigate("Firsthome")}
-      >
-        <Image
-          source={require("../../assets/splash.png")}
-          style={{ height: height, width: width / 1, marginTop: height / 19 }}
-        />
-      </TouchableHighlight>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+    <TouchableOpacity onPress={()=> navigation.replace('Firsthome')}>
+    <LinearGradient
+        // Background Linear Gradient
+        colors={['rgba(245,145,46,0.09)', 'transparent']}
+        style={styles.background}
+      />
+      <Image source={require("../../assets/uLeaner.png")} />
+    </TouchableOpacity>
+     
+     
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'orange',
+  },
+  background: {
+    position: 'absolute',
+    left: -80,
+    right: -80,
+    top: 10,
+    height: 350,
+  },
+});
